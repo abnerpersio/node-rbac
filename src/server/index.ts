c6eaa7591445fa320823de71945f5e5638abf6d2
Mateus Silva <mateusilva.dev@gmail.com>
2024-02-20 13:34:53 -0300

    live code

---
+++ src/server/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+
+import { routeAdapter } from './adapters/routeAdapter';
+
+import { makeAuthenticationMiddleware } from '../factories/makeAuthenticationMiddleware';
+import { makeListLeadsController } from '../factories/makeListLeadsController';
+import { makeSignInController } from '../factories/makeSignInController';
+import { makeSignUpController } from '../factories/makeSignUpController';
+import { middlewareAdapter } from './adapters/middlewareAdapter';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/sign-up', routeAdapter(makeSignUpController()));
+app.post('/sign-in', routeAdapter(makeSignInController()));
+
+app.get('/leads',
+  middlewareAdapter(makeAuthenticationMiddleware()),
+  routeAdapter(makeListLeadsController())
+);
+
+app.listen(3001, () => {
+  console.log('Server started at http://localhost:3001');
+});
